5293a215651a5b069320484b3e05349a283bea16
ongomobile <ongomobile@gmail.com>
2021-10-07 16:20:02 -0700

    Add onCreateWebpackConfig so Firebase can be used in node & browser environments

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,15 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.com/docs/node-apis/
- */
-
-// You can delete this file if you're not using it
+exports.onCreateWebpackConfig = ({ actions, stage }) => {
+  if (stage === "develop-html" || stage === "build-html") {
+    actions.setWebpackConfig({
+      resolve: {
+        mainFields: ["main"],
+      },
+    })
+  } else {
+    actions.setWebpackConfig({
+      resolve: {
+        mainFields: ["browser", "module", "main"],
+      },
+    })
+  }
+}
