2bebd118fa6a399ca79039e3401a55134d553a25
ongomobile <ongomobile@gmail.com>
2021-10-07 19:56:58 -0700

    Refactor header to use styled components

--- src/components/header.js
+++ src/components/header.js
@@ -1,35 +1,77 @@
 import * as React from "react"
 import PropTypes from "prop-types"
 import { Link } from "gatsby"
+import styled from "styled-components"
 
-const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
-  </header>
-)
+const LogoutLink = styled.span`
+  color: white;
+  cursor: pointer;
+  &:hover {
+    text-decoration: underline;
+  }
+`
+
+const HeaderWrapper = styled.header`
+  background: rebeccapurple;
+  margin-bottom: 1.45rem;
+`
+
+const HeaderContent = styled.div`
+  margin: 0 auto;
+  max-width: 960px;
+  padding: 1.45rem 1.0875rem;
+  display: flex;
+  > h1 {
+    margin: 0;
+    flex-grow: 1;
+    > a {
+      color: white;
+      text-decoration: none;
+    }
+  }
+  > div {
+    margin: auto 0;
+  }
+`
+
+const UserInfo = styled.div`
+  text-align: right;
+  color: white;
+`
+
+const LoginLink = styled.div`
+  margin: auto 0;
+  a {
+    color: white;
+  }
+`
+
+const Divider = styled.span`
+  margin: 0 8px;
+  padding-right: 1px;
+  background: #ddd;
+`
+
+const AdminLink = styled.span`
+  a {
+    color: white;
+  }
+`
+
+const Header = ({ siteTitle }) => {
+  return (
+    <HeaderWrapper>
+      <HeaderContent>
+        <h1>
+          <Link to="/">{siteTitle}</Link>
+        </h1>
+        <div>
+          <UserInfo>User Info</UserInfo>
+        </div>
+      </HeaderContent>
+    </HeaderWrapper>
+  )
+}
 
 Header.propTypes = {
   siteTitle: PropTypes.string,
