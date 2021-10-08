0bdfb19318aacb32a6ece11f9f1f5dee9535a642
ongomobile <ongomobile@gmail.com>
2021-10-07 18:34:40 -0700

    Add Firebase config object and environment variables
    Note more configuration may be needed

---
+++ src/components/Firebase/config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.GATSBY_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.GATSBY_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.GATSBY_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.GATSBY_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.GATSBY_FIREBASE_APP_ID,
+}
+
+export default firebaseConfig
